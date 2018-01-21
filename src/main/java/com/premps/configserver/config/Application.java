34c620b29942657e27262d94a6ee0449a15794df
Prempeh Gyan <gyan.prempeh@aiesec.net>
2018-01-21 17:41:53 +0100

    comments and descriptions added

--- src/main/java/com/premps/configserver/config/Application.java
+++ src/main/java/com/premps/configserver/config/Application.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * This class is the starting point of configuring and setting up the Spring
+ * Cloud Config Server. <br>
+ * By annotating the class with the @EnableConfigServer annotation, Spring
+ * automatically does the set up of the server hence this and the properties in
+ * the bootstrap.yml file are the only things needed to be done for the server
+ * to be functional.
+ * 
+ * @author Prempeh Gyan
+ * @version 1.0
+ * 
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
