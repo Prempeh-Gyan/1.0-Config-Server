cc95930f330403f65703a6783804064ad7a91e6a
Prempeh Gyan <gyan.prempeh@aiesec.net>
2018-02-03 15:01:57 +0100

    test for GitHub repo

--- src/test/java/com/premps/configserver/config/ApplicationTests.java
+++ src/test/java/com/premps/configserver/config/ApplicationTests.java
@@ -1,9 +1,17 @@
 package com.premps.configserver.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,9 +19,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
+	@Value("${spring.cloud.config.server.git.uri}")
+	private String gitHubConfigRepo = "";
+
 	@Test
 	public void contextLoads() {
+
 		assertTrue(true);
-	} 
+
+		String URL = gitHubConfigRepo;
+
+		try {
+			System.out.println(gitHubConfigRepo);
+			assertThat(getResponseCode(gitHubConfigRepo) != 404);
+		} catch (MalformedURLException e) {
+			fail(e.getMessage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	public static int getResponseCode(String urlString) throws MalformedURLException, IOException {
+		URL u = new URL(urlString);
+		HttpURLConnection huc = (HttpURLConnection) u.openConnection();
+		huc.setRequestMethod("GET");
+		huc.connect();
+		return huc.getResponseCode();
+	}
 
 }
